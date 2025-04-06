571718e15c719ce9938cdd78f681740f77b75b45
yahALFA <yah0note9@gmail.com>
2025-04-06 23:40:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def mikrotik_script():
     return Response(script, mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
